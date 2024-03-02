786211118acaaf9d04a76105c5dd24efce8c3f12
carlkaziboni <carlkazibonicman@hotmail.com>
2024-03-02 15:50:33 +0000

    db changes, app.py changes, index changes

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, abort, request
 from flask_session import sessions
 from flask_login import login_user, logout_user, login_required
-from werkzeug import utils, security
+from flask_mail import Mail
+from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 #landing page for app, so probably Introduction, link to login page, link to sign up page
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+    abort(400)
