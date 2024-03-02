7a795528972b63a81a48c889488a9233e3702c27
carlkaziboni <carlkazibonicman@hotmail.com>
2024-03-02 14:35:00 +0000

    Few hmtl pages, index with bootsrap navbar, app.py name index function

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_session import sessions
+from flask_login import login_user, logout_user, login_required
+from werkzeug import utils, security
 
 app = Flask(__name__)
 
-@app.route("/test")
-def hello_world():
-    return "<p>Hello, World!</p>"
+#landing page for app, so probably Introduction, link to login page, link to sign up page
+@app.route("/")
+def index():
+    return render_template("index.html")
