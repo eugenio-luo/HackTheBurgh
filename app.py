b819f93931259c4fdd19e1bad20cd071cb84346a
sjl-03 <shenjunlu03@gmail.com>
2024-03-02 16:38:55 +0000

    fridgehome

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, redirect, abort, request
-from flask_session import sessions
-from flask_login import login_user, logout_user, login_required
-from flask_mail import Mail
-from werkzeug.utils import secure_filename
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
+# from flask_session import sessions
+# from flask_login import login_user, logout_user, login_required
+# from flask_mail import Mail
+# from werkzeug.utils import secure_filename
+# from werkzeug.security import generate_password_hash, check_password_hash
+
+con = sqlite3.connect("FoodDB.db")
 
 app = Flask(__name__)
 
@@ -12,4 +17,13 @@ app = Flask(__name__)
 def index():
     if request.method == 'GET':
         return render_template("index.html")
+    abort(400)
+
+@app.route("/fridgehome", methods=['GET'])
+def fridge_home():
+    if request.method == 'GET':
+      cur = con.cursor()
+      cur.execute("SELECT Food, Quantity, ExpirationDate, PredictedExpirationDate, Photo FROM food")
+      items = cur.fetchall()
+      return render_template('fridgehome.html', items=items)
     abort(400)
